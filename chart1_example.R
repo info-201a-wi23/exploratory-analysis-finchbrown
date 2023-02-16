c0a3b2335c741427c631605f5b321811b92621bb
ypaulos <ypaulos@uw.edu>
2023-02-15 16:49:16 -0800

    saved dataset into variable

--- chart1_example.R
+++ chart1_example.R
@@ -1,3 +1,3 @@
 library(ggplot2)
 
-respondent_info_df <- read.csv("", stringsAsFactors = FALSE)
+respondent_info_df <- read.csv("Seattle_Votes_Survey_Data_from_5_224_immigrant_and_refugee_residents_of_Seattle_King_County.csv", stringsAsFactors = FALSE)
