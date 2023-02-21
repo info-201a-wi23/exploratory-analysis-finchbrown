dbb6b4be026d1694412ec22005252084927787d7
mofar6 <mofar6@uw.edu>
2023-02-20 22:32:38 -0800

    Bar graph

--- Making a difference scatplot.R
+++ Making a difference scatplot.R
@@ -9,7 +9,7 @@ respondent_info_df <- read.csv("Seattle_Votes_Survey_Data_from_5_224_immigrant_a
 differences_values <- respondent_info_df %>% count(Q08..Overall..how.much.of.a.difference.do.you.think.you.can.have.in.making.your.neighborhood.a.better.place.to.live., name = "Age")
 
 
-# Make a scatterplot
+# Make a bar graph
 ggplot(differences_values, aes(x = Q08..Overall..how.much.of.a.difference.do.you.think.you.can.have.in.making.your.neighborhood.a.better.place.to.live. y = Age)) + 
-  geom_point(size = 3, shape = 21, fill = "blue", color = "black") +
+  geom_bar(stat = "identity", fill = "blue") + 
   labs(title = "How much of a difference do you think you can make in your neighborhood", x = "Age", y = "Occurances")
