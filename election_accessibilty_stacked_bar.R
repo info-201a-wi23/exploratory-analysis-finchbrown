6b498339d3a635ceef0349be3b0dd74b0bd493f2
ypaulos <ypaulos@uw.edu>
2023-02-20 20:49:22 -0800

    added to stacked bar

--- election_accessibilty_stacked_bar.R
+++ election_accessibilty_stacked_bar.R
@@ -4,4 +4,5 @@ library(dplyr)
 
 ggplot(eis_survey_options, aes( x = information_sources, y = occurences)) + 
   geom_bar(position="stack", stat="identity") +
-  labs(title = "Stacked Bar Graph of News Sources" )
+  labs(title = "Stacked Bar Graph of News Sources", x = "Sources"
+       , y = "Occurences")
