d489437da92d4cf2d52d2ff73c73ea818e314e6b
ypaulos <ypaulos@uw.edu>
2023-02-20 22:06:46 -0800

    fixed typo

--- election_accessibilty_stacked_bar.R
+++ election_accessibilty_stacked_bar.R
@@ -1,12 +1,13 @@
 library(tidyverse)
 library(stringr)
 library(dplyr)
+library(ggplot2)
 
 # ggplot(eis_survey_options, aes( x = information_sources, y = Occurrences)) + 
 #   geom_bar(position="stack", stat="identity") +
 #   labs(title = "Stacked Bar Graph of News Sources", x = "Sources", y = "Occurrences")
 
-ggplot(eis_survey_options, aes(x="", y= occurrences, fill= information_sources)) +
+ggplot(eis_survey_options, aes(x="", y= occurences, fill= information_sources)) +
   geom_bar(stat="identity", width=1) +
   coord_polar("y", start=0) +
   labs(title = "Information Sources of Elections")
