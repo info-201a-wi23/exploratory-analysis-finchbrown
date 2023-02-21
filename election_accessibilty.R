4653c5599a8d4ae5197ea5679ffee776fea981da
ypaulos <ypaulos@uw.edu>
2023-02-20 19:06:51 -0800

    updating

--- election_accessibilty.R
+++ election_accessibilty.R
@@ -2,9 +2,5 @@ library(tidyverse)
 library(stringr)
 library(dplyr)
 
-current_year <- -2015
-
-avg_age <- respondent_info_df %>% group_by(respondent_info_df$Q02..In.what.year.were.you.born.) 
-  %>% summarize(avg_age = mean(respondent_info_df$Q02..In.what.year.were.you.born.) + current_year)
-  
-  
+ggplot(data, aes(fill = eis_survey_options, y = count(), x = unique_eis)) + 
+  geom_bar(position="stacked", stat="identity")
