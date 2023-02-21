0826a600ad2af545632a7e30f22daf8310feb66d
ypaulos <ypaulos@uw.edu>
2023-02-21 14:10:40 -0800

    color pallette

--- Household_income_histogram.R
+++ Household_income_histogram.R
@@ -12,8 +12,8 @@ unique_income_brackets <- respondent_info_df1 %>% count(Q24..What.was.your.total
 
 # Create the histogram
 ggplot(unique_income_brackets, aes(x = Q24..What.was.your.total.combined.household.income.in.2015.before.taxes., y = count)) +
-  geom_histogram(stat = "identity", fill = "blue", alpha = 0.8) +
+  geom_histogram(stat = "identity", alpha = 0.8) +
   labs(title = "Histogram of Household Income in 2015",
        x = "Household Income in 2015",
-       y = "Occurences")
+       y = "Occurences") + scale_fill_brewer(palette = "Blues")
 
