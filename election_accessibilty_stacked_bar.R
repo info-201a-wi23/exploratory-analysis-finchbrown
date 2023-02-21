d46a9683ff39e69be2fcde4e05f48cf46b87afde
ypaulos <ypaulos@uw.edu>
2023-02-20 21:18:21 -0800

    added pie chart

--- election_accessibilty_stacked_bar.R
+++ election_accessibilty_stacked_bar.R
@@ -2,7 +2,10 @@ library(tidyverse)
 library(stringr)
 library(dplyr)
 
-ggplot(eis_survey_options, aes( x = information_sources, y = occurances)) + 
-  geom_bar(position="stack", stat="identity") +
-  labs(title = "Stacked Bar Graph of News Sources", x = "Sources", y = "Occurances")
+# ggplot(eis_survey_options, aes( x = information_sources, y = occurences)) + 
+#   geom_bar(position="stack", stat="identity") +
+#   labs(title = "Stacked Bar Graph of News Sources", x = "Sources", y = "Occurences")
 
+ggplot(eis_survey_options, aes(x="", y= occurences, fill= information_sources)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0)
